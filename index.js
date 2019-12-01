b0e7bfdb22beb68e25a4f62d4330315332478ac3
Jakub Kozlowski <jakoz123@gmail.com>
2019-12-01 20:58:25 +0100

    Import routes to index.js

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const mongoose = require('mongoose');
+const debug = require('debug')('de:startup');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const config = require('config');
-const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+const products = require('./routes/products');
+const meals = require('./routes/meals');
 const express = require('express');
-
 const app = express();
+app.use(helmet());
+
+if(app.get('env') === 'development') {
+  app.use(morgan('dev'));
+  debug('Morgan enabled...');
+}
+
+mongoose.connect(config.get('db.host'), {useUnifiedTopology: true, useNewUrlParser: true})
+  .then(() => debug('Connected...'))
+  .catch(err => debug('Not connected: '+err));
+
 app.use(express.json());
+app.use('/api/products', products);
+app.use('/api/meals', meals);
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+app.listen(port, () => debug(`Listening on port ${port}...`));
